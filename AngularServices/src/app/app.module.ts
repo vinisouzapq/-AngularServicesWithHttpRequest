995afa999a67e26fa5e428939714c364057aa068
Marcos Caldas <mvinicius112015@gmail.com>
2020-05-06 22:51:58 -0300

    Criando os componentes, exercício 2

---
+++ AngularServices/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    UserComponent,
+    PostComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
